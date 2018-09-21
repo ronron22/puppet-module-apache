01fc1c0761771a618fbee520df7aa5fa5d66ed0f
ronron <ant@n.io>
2018-09-21 11:18:06 +0200

    .

--- manifests/cv.pp
+++ manifests/cv.pp
@@ -1,11 +1,11 @@
 class apache::cv (
 	$nodename = hiera('nodename'),
-	) {
-	file { '/var/www/html/antonio/cv/antonio-cv.pdf':
-		ensure    => present,
-		owner     => antonio,
-		group     => antonio,
-		mode      => '0644',
-		content   => file("apache/${nodename}/var/www/html/antonio/cv/antonio-cv.pdf"),
+	) inherits {
+		file { '/var/www/html/antonio/cv/antonio-cv.pdf':
+			ensure    => present,
+			owner     => antonio,
+			group     => antonio,
+			mode      => '0644',
+			content   => file("apache/${nodename}/var/www/html/antonio/cv/antonio-cv.pdf"),
 	}
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,10 +1,12 @@
-class apache::services (
-    $enable = ::apache::params::enable,
-    ) {
-    service { 'apache2':
-      ensure  => running,
-      enable  => true,
-      restart => 'apache2ctl -t && systemctl reload apache2',
-      require => File['/etc/apache2/apache2.conf'],
-    }
+class apache::service (
+
+	$enable = ::apache::params::enable,
+
+	) inherits apache {
+		service { 'apache2':
+			ensure  => running,
+			enable  => true,
+			restart => 'apache2ctl -t && systemctl reload apache2',
+			require => File['/etc/apache2/apache2.conf'],
+		}
 }
