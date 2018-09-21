d2e40c31ad7232126baa2fe186981ec01c9253cf
ronron <ant@n.io>
2018-09-21 12:52:19 +0200

    .

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class apache {
 	include apache::install
-	include apache::config
 	include apache::modules
+	include apache::config
 	include apache::vhosts
 	include apache::service
-	Class['apache::install'] ~> Class['apache::modules'] ~> Class['apache::config'] ~> Class['apache::vhosts'] ~> Class['apache::service']
+	#Class['apache::install'] ~> Class['apache::modules'] ~> Class['apache::config'] ~> Class['apache::vhosts'] ~> Class['apache::service']
 }
